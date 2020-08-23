94af6465a59a45ba224d1f93f7122f06741dbaf8
Arpit Chakladar <54011232+Arpit-creator@users.noreply.github.com>
2020-08-23 21:49:29 +0530

    removed json-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-const webpack = require('webpack');
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = env => {
-	const ENV = env?.NODE_ENV || 'development';
+	const ENV = env?.NODE_ENV || "development";
 
 	return {
 		context: path.resolve(__dirname, "src"),
-		entry: './index.tsx',
+		entry: "./index.tsx",
 
 		output: {
 			path: path.resolve(__dirname, "build"),
-			publicPath: '/',
-			filename: 'bundle.js'
+			publicPath: "/",
+			filename: "bundle.js"
 		},
 
 		optimization: {
-			minimize: ENV === 'production'
+			minimize: ENV === "production"
 		},
 
 		resolve: {
-			extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.scss'],
+			extensions: [".js", ".jsx", ".ts", ".tsx", ".json", ".scss"],
 			modules: [
 				path.resolve(__dirname, "src/lib"),
 				path.resolve(__dirname, "node_modules"),
-				'node_modules'
+				"node_modules"
 			],
 			alias: {
-				'@': path.resolve(__dirname, "src"),
-				'react': 'preact/compat',
-				'react-dom': 'preact/compat'
+				"@": path.resolve(__dirname, "src"),
+				"react": "preact/compat",
+				"react-dom": "preact/compat"
 			}
 		},
 
@@ -40,46 +40,42 @@ module.exports = env => {
 				{
 					test: /\.tsx?$/,
 					exclude: /node_modules/,
-					loaders: ['ts-loader']
+					loaders: ["ts-loader"]
 				},
 				{
 					test: /\.(scss|css)$/,
 					use: ExtractTextPlugin.extract({
-						fallback: 'style-loader',
-						use: ['css-loader', 'sass-loader']
+						fallback: "style-loader",
+						use: ["css-loader", "sass-loader"]
 					})
 				},
 				{
-					test: /\.json$/,
-					use: 'json-loader'
-				},
-				{
 					test: /\.(xml|html|txt|md)$/,
-					use: 'raw-loader'
+					use: "raw-loader"
 				},
 				{
 					test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif|mp3)(\?.*)?$/i,
-					use: ENV ==='production' ? 'file-loader' : 'url-loader'
+					use: ENV ==="production" ? "file-loader" : "url-loader"
 				}
 			]
 		},
 		plugins: ([
 			new webpack.NoEmitOnErrorsPlugin(),
 			new ExtractTextPlugin({
-				filename: 'style.css',
+				filename: "style.css",
 				allChunks: true,
 				disable: false
 			}),
 			new webpack.DefinePlugin({
-				'process.env.NODE_ENV': JSON.stringify(ENV)
+				"process.env.NODE_ENV": JSON.stringify(ENV)
 			}),
 			new HtmlWebpackPlugin({
-				template: path.join(__dirname, 'public', 'index.html'),
+				template: path.join(__dirname, "public", "index.html"),
 				minify: { collapseWhitespace: true }
 			}),
 			new CopyWebpackPlugin({
 				patterns: [
-					{ from: path.join(__dirname, 'public'), to: './' }
+					{ from: path.join(__dirname, "public"), to: "./" }
 				]
 			})
 		]),
